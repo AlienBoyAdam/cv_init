6525c7b4769ec69ba0242d3fdf4d72d7996e2f85
AlienBoyAdam <74253680+AlienBoyAdam@users.noreply.github.com>
2021-01-26 17:30:57 +0800

    Create findcard.py

---
+++ findcard.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('cv_init/Card/card.jpg')
+
+width, height = 250, 350
+
+# define four conor points
+pts1 = np.float32([[175, 73], [338, 106], [124, 299], [289, 334]])
+# define the original point
+pts2 = np.float32([[0,0],[width, 0],[0, height],[width, height]])
+# transformation matrix
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
+cv2.imshow('image', img)
+cv2.imshow('output', imgOutput)
+
+cv2.waitKey(0)
